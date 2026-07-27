7b31e917a59ad6b3d3ff9b634aeeb15f2ac25cf2
AMAN <amanpareek155@gmail.com>
2026-07-27 23:57:28 +0530

    Remove local-only files for Vercel deployment

--- rename_assets.ps1
+++
@@ -1,8 +0,0 @@
-$items = Get-ChildItem "e:\mafia\assets" -Filter "*.jpg"
-$i = 1
-foreach ($item in $items) {
-    $dest = "e:\mafia\assets\bg_$i.jpg"
-    Copy-Item -Path $item.FullName -Destination $dest -Force
-    Write-Host "Copied $($item.Name) -> bg_$i.jpg"
-    $i++
-}
